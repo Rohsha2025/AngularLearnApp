7025536803f2575d105712af9e015bfe5759c672
rohsha2025 <personal.perosnal.1@gmail.com>
2026-05-01 14:44:15 +0530

    event binding changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,6 +12,7 @@ export class App {
   protected readonly title = signal('AngularLearnApp');
   name = "kapil shamra";
   age = 27;
+  counter = 0;
 
   getName() {
     return this.name;
@@ -21,4 +22,25 @@ export class App {
     //need to define  datatype of a,b,c as this is typescript and it is a statically typed language
     return a + b + c;
   }
+
+  // we can also define the type of return value of a function in typescript
+  getValueWithReturnType(a: number, b: number, c: number): number {
+    return a + b + c;
+  }
+
+  //counterOps(action: string) {
+  //  if (action === 'increment') {
+  //    this.counter++;
+//  } we can define function like this but it is not a 
+// good practice as we can pass any string value to the function 
+// and it will not work as expected
+  counterOps(operation: 'increment' | 'decrement') {
+    if (operation === 'increment') {
+      this.counter++;
+    } else {
+      this.counter--;
+    }
+    alert("Counter: " + this.counter);
+  }
+
 }
