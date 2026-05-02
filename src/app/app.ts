a214607d292ef780e712d02b6c28e4e06a51b709
rohsha2025 <personal.perosnal.1@gmail.com>
2026-05-02 14:10:42 +0530

    add datatype codes

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,8 +12,30 @@ export class App {
   name = "kapil shamra";
   age = 27;
 
+  //if specifically want to give string
+  data:string = "example";
+
+  //if give two datatypes to a variable then it is called union type
+  example_1:string | number = "example"; // this means data can be either a string or a number 
+
+  //but it is very difficult to manage like there is more number of datatypes then it will be very difficult to
+  //  manage so we can use any datatype but it is not recommended as it will defeat the purpose of using typescript
+  example_2:any = "example"; // this means data can be of any type but it is not recommended as it will defeat the 
+  // purpose of using typescript
+
   getName() {
     return this.name;
+  }
+
+  getData() {
+    //this.data = 21; 
+    // This will cause a type error because 'data' is defined as a string, but we are trying to assign a number to it.
+    return this.data;
+  }
+
+  getexample_1() {
+    this.example_1 = 21;
+    return this.example_1;
   }
 
   getValue(a: number, b: number, c: number) {
