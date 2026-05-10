f77a94ddfdfec797a72c1a08355ec854ca6bfd82
rohsha2025 <personal.perosnal.1@gmail.com>
2026-05-10 16:22:03 +0530

    changes related to making of counter app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,15 +9,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('AngularLearnApp');
-  name = "kapil shamra";
-  age = 27;
+  
+  count: WritableSignal<number> = signal(0);
 
-  getName() {
-    return this.name;
-  }
-
-  getValue(a: number, b: number, c: number) {
-    //need to define  datatype of a,b,c as this is typescript and it is a statically typed language
-    return a + b + c;
+  operation(param: string) {
+    if (param === 'add') {
+      this.count.update(value => value + 1);
+    } else if (param === 'subtract') {
+      this.count.update(value => value - 1);
+      if(this.count() < 0) {
+        this.count.set(0);
+        alert('Count cannot be negative, resetting to 0. Please try increment opearation instead.');
+      }
+    } else if (param === 'reset') {
+      this.count.set(0);
+    } else {
+      console.log('Invalid operation');
+    }
   }
 }
