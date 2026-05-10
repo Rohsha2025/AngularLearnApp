3cdab7bf1f94fdca0ff9851bdb949835fd25aeb0
rohsha2025 <personal.perosnal.1@gmail.com>
2026-05-10 16:23:20 +0530

    main branch

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,15 +9,4 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('AngularLearnApp');
-  name = "kapil shamra";
-  age = 27;
-
-  getName() {
-    return this.name;
-  }
-
-  getValue(a: number, b: number, c: number) {
-    //need to define  datatype of a,b,c as this is typescript and it is a statically typed language
-    return a + b + c;
-  }
 }
